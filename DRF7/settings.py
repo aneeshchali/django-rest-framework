9d545c6c13c67b73776254f3863c0998539a47a4
aneeshchali <chali.aneesh@gmail.com>
2023-02-20 13:32:20 +0530

    doing serializers in the main branch

--- DRF7/settings.py
+++ DRF7/settings.py
@@ -127,9 +127,10 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
     ]
 }
--- api/views.py
+++ api/views.py
@@ -3,26 +3,28 @@ import json
 from django.http import JsonResponse,HttpResponse
 from django.forms.models import model_to_dict
 
+
+##Django rest frameowrk responsr is shown below:
+from products.serializers import ProductSerializer
+from rest_framework.response import Response
+from rest_framework.decorators import  api_view
 # Create your views here.
 from products.models import Product
 
+@api_view(["GET"])
 def api_home(request):
+    """
 
-    model_data = Product.objects.all().order_by('?').first() # random query set from ?
+    :param normal request:
+    :return: returning model imple property data:
+    """
+    instance = Product.objects.all().order_by('?').first() # random query set from ?
     data = {}
 
-    if model_data:
-        # data['id'] = model_data.id
-        # data['title'] = model_data.title
-        # data['content'] = model_data.content
-        # data['price'] = model_data.price
-
-        #model instance (model_data)
-        #turn into a py dict
-        #return json
-#----------------------------------------------------
-        #below works same as above in this scenerio.
-        data = model_to_dict(model_data,fields=['id','title','price','content'])
-        json_data = json.dumps(data)
+    if instance:
+        # data = model_to_dict(model_data,fields=['id','title','price','content'])
+        data =  ProductSerializer(instance).data
+        print(data)
+        return  Response(data)
             #default content_type of httpResponse is text/str
-    return  HttpResponse(data,headers={'content-type':'application/json'})
+    # return  HttpResponse(data,headers={'content-type':'application/json'})
